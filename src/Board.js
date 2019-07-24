67e49cf58724ac7274d13b4a29fde66a37387d97
Ryan Grunest <ryangrunest45@gmail.com>
2019-07-24 15:37:37 -0700

    table displays

--- src/Board.js
+++ src/Board.js
@@ -34,23 +34,30 @@ class Board extends Component {
   static defaultProps = {
     nrows: 5,
     ncols: 5,
-    chanceLightStartsOn: 0.3
-  }
+    chanceLightStartsOn: 0.25
+  };
 
   constructor(props) {
     super(props);
     this.state = {
       hasWon: false,
       board: this.createBoard()
-    }
-  }
+    };
+  };
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
-    return board
-  }
+    for (let i = 0; i < this.props.nrows; i++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++)  {
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row);
+    }
+    return board;
+  };
 
   /** handle changing a cell: update board & determine if winner */
   flipCellsAround(coord) {
@@ -85,20 +92,30 @@ class Board extends Component {
         <h1>You won!</h1>
       )
     } else {
-      // make table board
+      // let table = this.state.board.map(row => {
+      //               return(<tr key={row}>
+      //                 {this.state.board[row].map(row => <Cell key={row}/>)}
+      //               </tr>)
+      // })
+
+      let tableBoard = [];
+      for (let y = 0; y < this.props.nrows; y++)  {
+        let row = [];
+        for (let x = 0; x < this.props.ncols; x++) {
+          let coord = `${y}-${x}`
+          row.push(<Cell key={coord} isLit={this.state.board[y][x]} />)
+        }
+        console.log(row);
+        tableBoard.push(<tr key={y}>{row}</tr>);
+      }
       return(
         <table className="Board">
-          <tr>
-            <Cell isLit="true" />
-            <Cell />
-            <Cell isLit="true"/>
-            <Cell />
-          </tr>
+          <tbody>
+            {tableBoard}
+          </tbody>
         </table>
       )
     }
-    
-    
   }
 }
 
